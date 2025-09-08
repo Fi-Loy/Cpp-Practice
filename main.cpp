14ae21fe9700686de200e56d9fda7cff33eb9f2a
Fi-Loy <david.brunner00@gmail.com>
2025-09-08 19:39:35 -0400

    Exercise 1.11
    Wite a program that prompts the user for two integers. Print each number in the range specified by those two integers.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 //Practice code for C++ Primer
-//Chapter: 1.2. A First Look at Input/Output
-/*Exercise 1.3 Write a program to print Hello World to the 
-standard output
+//Chapter: 1.4.1 The while Statement
+/* Exercise 1.11: Write a program that prompts the user for 
+two integers. Print each number in the range specified by
+those two integers
 */
-
 #include <iostream>
 
 int main(){
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Enter two integers: \n";
+    int i1;
+    int i2;
+
+    std::cin >> i1 >> i2;
+    
+    int lb, ub;
+    lb = i1 < i2 ? i1 : i2;
+    ub = i1 < i2 ? i2 : i1;
+
+    while (lb <= ub){
+        std::cout << lb <<std::endl;
+        lb++;
+    }
 }
 
 //TODO and Exercises
-//Exercise 1.3
+//Exercise 1.11 
+//Exercise 1.3 DONE
